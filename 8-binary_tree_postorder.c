9150faec1099c6ada8d28843376ec46ce6a561ce
Naod Taddese <naodtds@gmail.com>
2024-02-01 12:31:54 +0300

    add postorder traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - this visits the tree in postorder traversal
+ * @tree: the tree to be visited
+ * @func: the function to print node's data
+ * Return: returns nothing or void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+	else
+		return;
+}

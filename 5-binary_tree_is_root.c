cb97afe04b31db63cb96954a6599d3086b6b57a8
Naod Taddese <naodtds@gmail.com>
2024-02-01 12:03:53 +0300

    check if node is parent

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - this function checks if node is root
+ * @node: the node to be checked
+ * Return: 1 or 0 (for true or false)
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL && node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

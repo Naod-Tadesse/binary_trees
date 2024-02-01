29d432d750c79b4d40674cac0248877c12ad7acf
Naod Taddese <naodtds@gmail.com>
2024-02-01 14:44:07 +0300

    number of leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - this counts the number of leaves
+ * @tree: the binary tree given
+ * Return: the number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t l = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		l = l + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	}
+
+	return (l);
+}

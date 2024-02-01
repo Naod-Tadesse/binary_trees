6d6587d5b20b2cb35f00cd735f9ab5361cded5f0
Naod Taddese <naodtds@gmail.com>
2024-02-01 17:55:10 +0300

    fix on finding uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,8 +13,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	}
 
-	if (node->parent->parent)
+	if (node->parent)
 	{
+		if (node->parent->parent == NULL)
+			return (NULL);
 		if (node->parent == node->parent->parent->right)
 		{
 			return (node->parent->parent->left);
